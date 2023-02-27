c95dd9ce88a4391db6d0694fb9207366080864aa
Lum-ona <lumonamulengwa@gmail.com>
2023-02-27 12:58:56 +0300

    Made tremendous change

--- main.py
+++ main.py
@@ -33,3 +33,26 @@ people = {
 }
 
 print(people["Dee"][1]['Bio']["Dob"])
+
+
+
+PEOPLE = {
+    "Lee": 43,
+
+    "Liz": [12,34,45],
+
+    "Vee": {
+        "place": "Rongai",
+        "School": "Dogomothi preparatory rehab center",
+        "phone": 7123456789
+        },
+
+    "Dee": [
+            {"SocialMedia": 'ig', "Personality": "extrovat"}, 
+            {"Bio":
+                 {"Dob": '12/53/5', "id": 452324}
+                 }
+                ]
+}
+
+print(people["Dee"][1]['Bio']["Dob"])

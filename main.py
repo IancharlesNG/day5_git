2477a519a93c8f6e5dc96f850857d7eea0e252e7
Lum-ona <lumonamulengwa@gmail.com>
2023-02-27 13:05:10 +0300

    .

--- main.py
+++ main.py
@@ -36,23 +36,6 @@ print(people["Dee"][1]['Bio']["Dob"])
 
 
 
-PEOPLE = {
-    "Lee": 43,
-
-    "Liz": [12,34,45],
-
-    "Vee": {
-        "place": "Rongai",
-        "School": "Dogomothi preparatory rehab center",
-        "phone": 7123456789
-        },
-
-    "Dee": [
-            {"SocialMedia": 'ig', "Personality": "extrovat"}, 
-            {"Bio":
-                 {"Dob": '12/53/5', "id": 452324}
-                 }
-                ]
-}
+PEOPLE = {'23423'}
 
 print(people["Dee"][1]['Bio']["Dob"])

81c92af4b37bb7397012e925c10d0f2d66746d93
Ian C <111271297+IancharlesNG@users.noreply.github.com>
2023-02-27 12:54:50 +0300

    liz,dee,vee and lee informtion

---
+++ main.py
@@ -0,0 +1,35 @@
+#Variables
+age = 12
+name = "Bob"
+#Or
+age, name = 12, 'Bob'
+
+#List
+names = ["Liz", "Lee", "Vee", "Dee"]
+ages = [12, 43, 56, 76]
+
+# for index, n in enumerate(names):
+#     print(f'Name: {n} Age: {ages[index]}')
+
+# dictionary
+
+people = {
+    "Lee": 43,
+
+    "Liz": [12,34,45],
+
+    "Vee": {
+        "place": "Rongai",
+        "School": "Dogomothi preparatory rehab center",
+        "phone": 7123456789
+        },
+
+    "Dee": [
+            {"SocialMedia": 'ig', "Personality": "extrovat"}, 
+            {"Bio":
+                 {"Dob": '12/53/5', "id": 452324}
+                 }
+                ]
+}
+
+print(people["Dee"][1]['Bio']["Dob"])
